b6f8b4358d95fe0d43030c40007df526cb344315
salim.azeri <salim.azeri@iteo.com>
2021-01-15 21:06:25 +0100

    add packages

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ClashRoyaleStatsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          alignment: Alignment.center,
+          child: const Text('app'),
+        ),
+      ),
+    );
+  }
+}
